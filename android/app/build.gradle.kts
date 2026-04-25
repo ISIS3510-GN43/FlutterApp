94535bf538cd2e0a3e05e0c08920d05ca28e0e3a
David Quiroga <de.quiroga2@uniandes.edu.co>
2026-04-25 04:28:54 -0500

    Firebase error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.app_flutter"
+    namespace = "com.techsolutions.worqee"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973" 
 
@@ -22,8 +22,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        namespace = "com.example.app_flutter"
-        applicationId = "com.example.app_flutter"
+        namespace = "com.techsolutions.worqee"
+        applicationId = "com.techsolutions.worqee"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
